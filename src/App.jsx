48454a5886c3670bf069272a6c3235c87803a649
shankstorm <work.prabhdeepsinngh@gmail.com>
2023-02-27 16:55:11 +0530

    commit after applying useSelector hook from react-redux to access the global store

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import { useSelector, useDispatch } from "react-redux";
 
 function App() {
   const dispatch = useDispatch();
+  const { url } = useSelector((state) => state.home);
+  console.log(url);
 
   const apiTesting = () => {
     fetchDataFromApi("/movie/popular").then((res) => {
-      console.log(res);
+      // console.log(res);
       dispatch(getApiConfiguration(res));
     });
   };
@@ -16,7 +18,7 @@ function App() {
   useEffect(() => {
     apiTesting();
   }, []);
-  return <div>App</div>;
+  return <div>{url?.total_pages}</div>;
 }
 
 export default App;
