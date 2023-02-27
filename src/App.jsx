e499348cb9d3d4342e843e7e99ac9289a8ab7866
shankstorm <work.prabhdeepsinngh@gmail.com>
2023-02-27 13:08:03 +0530

    commit after creating axios fetch functionality

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React, { useEffect } from "react";
+import { fetchDataFromApi } from "./utils/api";
+
+function App() {
+  const apiTesting = () => {
+    fetchDataFromApi("/movie/popular").then((data) => {
+      console.log(data);
+    });
+  };
+
+  useEffect(() => {
+    apiTesting();
+  }, []);
+  return <div>App</div>;
+}
+
+export default App;
