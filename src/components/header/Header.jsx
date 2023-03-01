7c451f917d4137094099b4433b7f5a7dab9f9f53
shankstorm <work.prabhdeepsinngh@gmail.com>
2023-03-01 11:28:35 +0530

    commit after completing header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -18,10 +18,28 @@ const Header = () => {
   const navigate = useNavigate();
   const location = useLocation();
 
-  const controlNavbar = () => {};
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+
+  const controlNavbar = () => {
+    if (window.scrollY > 200) {
+      if (window.scrollY > lastScrollY && !mobileMenu) {
+        setShow("hide");
+      } else {
+        setShow("show");
+      }
+    } else {
+      setShow("top");
+    }
+    setLastScrollY(window.scrollY);
+  };
 
   useEffect(() => {
     window.addEventListener("scroll", controlNavbar);
+    return () => {
+      window.removeEventListener("scroll", controlNavbar);
+    };
   }, [lastScrollY]);
 
   const openSearch = () => {
