22d14af01d9032f7f6b2173f6eeb493ee40e061d
shankstorm <work.prabhdeepsinngh@gmail.com>
2023-03-01 11:07:47 +0530

    commit before header transition effect

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -18,20 +18,95 @@ const Header = () => {
   const navigate = useNavigate();
   const location = useLocation();
 
+  const controlNavbar = () => {};
+
+  useEffect(() => {
+    window.addEventListener("scroll", controlNavbar);
+  }, [lastScrollY]);
+
+  const openSearch = () => {
+    setMobileMenu(false);
+    setShowSearch(true);
+  };
+  const openMobileMenu = () => {
+    setMobileMenu(true);
+    setShowSearch(false);
+  };
+
+  const searchQueryHandler = (e) => {
+    if (e.key === "Enter" && query.length > 0) {
+      navigate(`/search/${query}`);
+      setTimeout(() => {
+        setShowSearch(false);
+      }, 1000);
+    }
+  };
+
+  const navigationHandler = (type) => {
+    if (type === "movie") {
+      navigate("/explore/movie");
+    } else {
+      navigate("/explore/tv");
+    }
+    setMobileMenu(false);
+  };
   return (
-    <header>
+    <header className={`header ${mobileMenu ? "mobileView" : ""} ${show} `}>
       <ContentWrapper>
         <div className="logo">
           <img src={logo} alt="" />
         </div>
         <ul className="menuItems">
-          <li className="menuItem">Movies</li>
-          <li className="menuItem">Tv Shows</li>
+          <li className="menuItem" onClick={() => navigationHandler("movie")}>
+            Movies
+          </li>
+          <li className="menuItem" onClick={() => navigationHandler("tv")}>
+            Tv Shows
+          </li>
           <li className="menuItem">
-            <HiOutlineSearch />
+            <HiOutlineSearch onClick={openSearch} />
           </li>
         </ul>
+
+        <div className="mobileMenuItems">
+          <HiOutlineSearch onClick={openSearch} />
+          {mobileMenu ? (
+            <VscChromeClose
+              style={{ cursor: "pointer" }}
+              onClick={() => {
+                setMobileMenu(false);
+              }}
+            />
+          ) : (
+            <SlMenu onClick={openMobileMenu} style={{ cursor: "pointer" }} />
+          )}
+        </div>
       </ContentWrapper>
+      {showSearch && (
+        <div className="searchBar">
+          <ContentWrapper>
+            <div className="searchInput">
+              <input
+                onKeyUp={searchQueryHandler}
+                type="text"
+                name=""
+                id=""
+                placeholder="Search for movies..."
+                value={query}
+                onChange={(e) => {
+                  setQuery(e.target.value);
+                }}
+              />
+              <VscChromeClose
+                style={{ cursor: "pointer" }}
+                onClick={() => {
+                  setShowSearch(false);
+                }}
+              />
+            </div>
+          </ContentWrapper>
+        </div>
+      )}
     </header>
   );
 };
