855f828b6d06e58b084a45565d364546351e8d74
shankstorm <work.prabhdeepsinngh@gmail.com>
2023-03-04 11:51:35 +0530

    commit after creating similar and recommendations component

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -7,6 +7,8 @@ import { useParams } from "react-router-dom";
 import DetailsBanner from "./detailsBanner/DetailsBanner";
 import Cast from "./cast/Cast";
 import VideosSection from "./videoSection/VideosSections";
+import Similar from "./carousels/Similar";
+import Recommendation from "./carousels/Recommendation";
 
 const Details = () => {
   const { mediaType, id } = useParams();
@@ -14,13 +16,15 @@ const Details = () => {
   const { data: credits, loading: creditsLoading } = useFetch(
     `/${mediaType}/${id}/credits`
   );
-  console.log(data);
+  // console.log(data);
 
   return (
     <div>
       <DetailsBanner video={data?.results[0]} crew={credits?.crew} />
       <Cast data={credits?.cast} loading={credits?.loading} />
       <VideosSection data={data} loading={loading} />
+      <Similar mediaType={mediaType} id={id} />
+      <Recommendation mediaType={mediaType} id={id} />
     </div>
   );
 };
