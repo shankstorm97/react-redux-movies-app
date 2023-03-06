dcf6742970ab8f2e4039908c7c9f503693662581
shankstorm <work.prabhdeepsinngh@gmail.com>
2023-03-06 14:50:50 +0530

    commit after correction to the header component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -22,12 +22,14 @@ const Footer = () => {
           <li className="menuItem">FAQ</li>
         </ul>
         <div className="infoText">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur.
+          The Bigshxnk Flix is a place where you can easily find all the latest
+          movies, Tv Shows and more without the hassle of going to google and
+          searching for latest stuff.
+          <br />
+          <span>
+            Moreover, this service fetches data from tmdb which updates every
+            day by people around the world.
+          </span>
         </div>
         <div className="socialIcons">
           <span className="icon">
